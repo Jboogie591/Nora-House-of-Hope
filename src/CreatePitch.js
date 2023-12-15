b259624aa87daed2a507305f9808127f57cd54e6
johnnyhall <jfh2345@yahoo.com>
2023-12-15 17:45:15 -0500

    forms created

--- src/CreatePitch.js
+++ src/CreatePitch.js
@@ -1,13 +1,68 @@
-import React from 'react'
 
-function CreatePitch(){
+import React,{useState,useEffect} from 'react'
+
+
+function CreatePitch(){ 
+
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState(""); 
+    const [amount, setAmount] = useState("");  
+    
+    const handleChange = (event) => {
+        if (event.target.name === "pitchTitle") {
+            setTitle(event.target.value)
+        }
+        else if (event.target.name === "pitchStory") {
+            setDescription(event.target.value)
+        }
+        else if (event.target.name === "AmountNeeded") {
+            setAmount(event.target.value)
+        }
+    }
+
+    const submitForm = (event) => {
+        event.preventDefault()
+    }
 
 
     return(
+
+    <div>
         <h1>
            CreatePitch
-        </h1>
+        </h1> 
+        <form>
+            <label for="pitchInput">Pitch Title:</label>
+            <input
+                type= "text"
+                name= "pitchTitle"
+                id="pitchInput"
+                value= {title}
+                onChange={handleChange}
+            />
+            <label for="pitchDesc">Pitch Description:</label>
+            <input
+                type= "text"
+                name= "pitchStory"
+                id="pitchDesc"
+                value={description}
+                onChange={handleChange}
+            />
+            <label for="amount">Amount Needed:</label>
+            <input
+                type= "text"
+                name= "AmountNeeded"
+                id="amount"
+                value={amount}
+                onChange={handleChange}
+            />
+            <button type="submit" onClick={submitForm}>Submit Pitch</button>
+            
+
+        </form>
+        </div>
     )
+
 
 }
 export default CreatePitch
--- src/Donate.js
+++ src/Donate.js
@@ -1,12 +1,71 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 
 function Donate(){
 
+    const [donation, setDonation] = useState("");
+    const [card, setCard] = useState(""); 
+    const [expiration, setExpiration] = useState("");  
+    
+    const handleChange = (event) => {
+        if (event.target.name === "donationAmount") {
+            setDonation(event.target.value)
+        }
+        else if (event.target.name === "cardNumber") {
+            setCard(event.target.value)
+        }
+        else if (event.target.name === "ExpirationDate") {
+            setExpiration(event.target.value)
+        }
+    }
+
+    const submitForm = (event) => {
+        event.preventDefault()
+    }
+
+
+
+
 
     return(
-        <h1>
+        
+        <div>
+            <h1>
            Donate
         </h1>
+
+        <form>
+        <label for="donAmount">Amount:</label>
+        <input
+            type= "text"
+            name= "donationAmount"
+            id="donAmount"
+            value= {donation}
+            onChange={handleChange}
+        />
+        <label for="cardNum">Card Number:</label>
+        <input
+            type= "text"
+            name= "cardNumber"
+            id="cardNum"
+            value={card}
+            onChange={handleChange}
+        />
+        <label for="exp">Expiration Date:</label>
+        <input
+            type= "text"
+            name= "ExpirationDate"
+            id="exp"
+            value={expiration}
+            onChange={handleChange}
+        />
+        <button type="submit" onClick={submitForm}>Submit Donation</button>
+    
+        
+        
+    
+        </form>
+    
+    </div>
     )
 
 }
